415145aeda725e87f426cef0595d32bcee233af6
Elie KADOCHE <eliekadoche78@gmail.com>
2020-05-03 13:26:27 +0200

    Completed base model structure

--- model_0.py
+++ model_0.py
@@ -6,14 +6,41 @@ from datetime import datetime
 
 import numpy as np
 import torch
-import torch.nn.functional as F
-import torch.nn.modules as module
+import torch.nn as nn
 from sklearn.model_selection import train_test_split
 
 import utils
 
+# General variables
+# ------------------------------------------
 
-def p(text):
+MODEL_NAME = "model_0"
+MODEL_PATH = "{}{}.pt".format(utils.MODELS_FOLDER, MODEL_NAME)
+device = None
+
+
+class Model0(nn.Module):
+    """Model class.
+
+    The model is a function taking as input a batch of strings and returning
+    a scalar for each sample, representing the sentiment level.
+    """
+
+    def __init__(self):
+        """Init function."""
+        super(Model0, self).__init__()
+
+    def forward(self, x):
+        """Forward function.
+
+        Args:
+            x (tensor): tensor of strings, shape (N,) with N the batch size
+            s (tensor): tensor of scalars, shape (N,) with N the batch size
+        """
+        pass
+
+
+def _p(text):
     """Text preprocessing for logging.
 
     Args:
@@ -29,59 +56,74 @@ def p(text):
 
 
 def create(args):
-    """Function to create a fresh new model_0.
+    """Create a fresh new model.
 
     This function does not train or test the model. It just creates it and
-    save it in models/model_0/models_0.pt.
+    save it in models/model_name/model_name.pt.
     """
-    logging.INFO(p("launching create function"))
+    logging.info(_p("launching create function"))
+
+    model = Model0().to(device)
+    torch.save(model, MODEL_PATH)
+
+    logging.info(_p("model created and saved in {}".format(MODEL_PATH)))
 
 
 def train(args):
-    """Function to train an existing model_0.
+    """Train an existing model.
 
-    This function trains the model models/model_0/models_0.pt. It does not use
-    the testing dataset, but only the training one. The model is updated
+    This function trains the model models/model_name/model_name.pt. It does not
+    use the testing dataset, but only the training one. The model is updated
     after each epoch.
 
     All operations on the training dataset are allowed.
     """
-    logging.INFO(p("launching train function"))
+    logging.info(_p("launching train function"))
 
     raw_x_train = np.load(utils.X_TRAIN_PATH, allow_pickle=True)
     raw_y_train = np.load(utils.Y_TRAIN_PATH)
-    logging.INFO(p("raw_x_train shape: {}".format(raw_x_train.shape)))
-    logging.INFO(p("raw_y_train shape: {}".format(raw_y_train.shape)))
+    logging.info(_p("raw_x_train shape: {}".format(raw_x_train.shape)))
+    logging.info(_p("raw_y_train shape: {}".format(raw_y_train.shape)))
 
     x_train, x_valid, y_train, y_valid = train_test_split(
         raw_x_train, raw_y_train, test_size=0.2, random_state=42)
-    logging.INFO(p("x_train shape: {}".format(x_train.shape)))
-    logging.INFO(p("y_train shape: {}".format(y_train.shape)))
-    logging.INFO(p("x_valid shape: {}".format(x_valid.shape)))
-    logging.INFO(p("y_valid shape: {}".format(y_valid.shape)))
+    logging.info(_p("x_train shape: {}".format(x_train.shape)))
+    logging.info(_p("y_train shape: {}".format(y_train.shape)))
+    logging.info(_p("x_valid shape: {}".format(x_valid.shape)))
+    logging.info(_p("y_valid shape: {}".format(y_valid.shape)))
+
+    # TODO
+    # train function
 
 
 def evaluate(args):
-    """Function to evaluate an existing model_0.
+    """Evaluate an existing model.
 
-    This function evaluates the model models/model_0/models_0.pt on the testing
-    dataset. Results are saved in results/model_0/.
+    This function evaluates the model models/model_name/model_name.pt on the
+    testing dataset. Results are saved in results/model_name/.
 
     The testing dataset must not be modified.
     """
-    logging.INFO(p("launching evaluate function"))
+    logging.info(_p("launching evaluate function"))
 
     x_test = np.load(utils.X_TEST_PATH, allow_pickle=True)
     y_test = np.load(utils.Y_TEST_PATH)
-    logging.INFO(p("x_test shape: {}".format(x_test.shape)))
-    logging.INFO(p("y_test shape: {}".format(y_test.shape)))
+    logging.info(_p("x_test shape: {}".format(x_test.shape)))
+    logging.info(_p("y_test shape: {}".format(y_test.shape)))
+
+    model = torch.load(MODEL_PATH).to(device)
+    model.eval()
+
+    # TODO
+    # evaluate function
 
 
 if __name__ == "__main__":
     # Logging
     # ------------------------------------------
     logging.basicConfig(
-        filename="{}model_0/model_0.log".format(utils.RESULTS_FOLDER),
+        filename="{}{}/{}.log".format(utils.RESULTS_FOLDER,
+                                      MODEL_NAME, MODEL_NAME),
         filemode="a",
         level=logging.DEBUG
     )
@@ -92,14 +134,15 @@ if __name__ == "__main__":
     utils.create_dir(utils.RESULTS_FOLDER, delete_old=False)
     utils.create_dir(utils.MODELS_FOLDER, delete_old=False)
 
-    utils.create_dir("{}model_0/".format(utils.RESULTS_FOLDER), False)
-    utils.create_dir("{}model_0/".format(utils.MODELS_FOLDER), False)
+    utils.create_dir("{}{}/".format(utils.RESULTS_FOLDER, MODEL_NAME), False)
+    utils.create_dir("{}{}/".format(utils.MODELS_FOLDER, MODEL_NAME), False)
 
     # Main parser
     # ------------------------------------------
 
     parser = argparse.ArgumentParser(
-        "model_0 for the sentiment analysis problem")
+        "{} for the sentiment analysis problem".format(MODEL_NAME))
+    parser.add_argument("--cuda", help="Using GPU or not", action="store_true")
     subparsers = parser.add_subparsers()
 
     # Create parser
@@ -107,7 +150,7 @@ if __name__ == "__main__":
 
     parser_create = subparsers.add_parser(
         "create",
-        help="Command to create a fresh new model_0",
+        help="Command to create a fresh new model",
     )
     parser_create.set_defaults(func=create)
 
@@ -116,8 +159,12 @@ if __name__ == "__main__":
 
     parser_train = subparsers.add_parser(
         "train",
-        help="Command to train model_0",
+        help="Command to train the model",
     )
+    parser_train.add_argument(
+        "--epochs", help="Number of epochs", default=42, type=int)
+    parser_train.add_argument(
+        "--batch-size", help="Batch size", default=128, type=int)
     parser_train.set_defaults(func=train)
 
     # Evaluate parser
@@ -125,7 +172,7 @@ if __name__ == "__main__":
 
     parser_evaluate = subparsers.add_parser(
         "evaluate",
-        help="Command to evaluate an existing model_0"
+        help="Command to evaluate an existing model"
     )
     parser_evaluate.set_defaults(func=evaluate)
 
@@ -133,4 +180,8 @@ if __name__ == "__main__":
     # ------------------------------------------
 
     args = parser.parse_args()
+
+    use_cuda = args.cuda and torch.cuda_is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
     args.func(args)
