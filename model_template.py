740e8f522ff308ed9ef671594a5aa96018aeb4c3
Elie KADOCHE <eliekadoche78@gmail.com>
2020-05-14 23:05:46 +0200

    Fixed bug

--- model_template.py
+++ model_template.py
@@ -106,6 +106,15 @@ def evaluate(args):
 
 
 if __name__ == "__main__":
+    # Creating necessary folders
+    # ------------------------------------------
+
+    utils.create_dir(utils.RESULTS_FOLDER, delete_old=False)
+    utils.create_dir(utils.MODELS_FOLDER, delete_old=False)
+
+    utils.create_dir("{}{}/".format(utils.RESULTS_FOLDER, MODEL_NAME), False)
+    utils.create_dir("{}{}/".format(utils.MODELS_FOLDER, MODEL_NAME), False)
+
     # Logging
     # ------------------------------------------
     logging.basicConfig(
@@ -116,15 +125,6 @@ if __name__ == "__main__":
         format="%(asctime)s - %(levelname)s: %(message)s",
         datefmt="%Y/%m/%d %H:%M:%S",
     )
-
-    # Creating necessary folders
-    # ------------------------------------------
-
-    utils.create_dir(utils.RESULTS_FOLDER, delete_old=False)
-    utils.create_dir(utils.MODELS_FOLDER, delete_old=False)
-
-    utils.create_dir("{}{}/".format(utils.RESULTS_FOLDER, MODEL_NAME), False)
-    utils.create_dir("{}{}/".format(utils.MODELS_FOLDER, MODEL_NAME), False)
 
     # Main parser
     # ------------------------------------------
